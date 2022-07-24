01b98c8d12d8badaeb082412bac53d526aa4b151
Aliyu gambo aliyu <gamboaliyu00@gmail.com>
2022-07-24 21:02:14 +0000

    Makes A change on the  Styles

--- app.mjs
+++ app.mjs
@@ -1,17 +1,45 @@
+import data from './data.mjs'
+
 function startApp() {
-    // Your entire app should not necessarily be coded inside this 
-    // single function (though there's no penalty for that), 
-    // so create and use/call additional functions from here
-  
-    // pls remove the below and make some magic in here!
-    console.log('make magic in here!');
-  
-    const header = document.querySelector('h2');
-    if(header) {
-      header.textContent = 'make some magic here!!';
+  // Your entire app should not necessarily be coded inside this 
+  // single function (though there's no penalty for that), 
+  // so create and use/call additional functions from here
+  // pls remove the below and make some magic in here!
+  console.log('make magic in here!');
+const screen = document.querySelector('.screen');
+
+  form.onsubmit = function(e) {
+    e.preventDefault();
+    var input_value = document.form.PhoneNumber.value;
+    //console.log(select);
+    input_value.addEventListener('keyup', checkCarrier)
+
+
+    function checkCarrier(e) {
+    let x = e.target.value;
+    // 
+    if (x.startsWith('+234')) {
+        x = x.replace('+234', '0')
     }
-  };
-  
-  // ======= DO NOT EDIT ============== //
-  export default startApp;
+    for (let item of Object.keys(num)) {
+        if (x.startsWith(item)) {
+            screen.innerHTML = `<img src=${num[item]} alt='logo'>`;
+
+        }
+
+    }
+
+}
+
+  }
+
+
+  const header = document.querySelector('h2');
+  if (header) {
+    header.textContent = 'make some magic here!!';
+  }
+};
+
+// ======= DO NOT EDIT ============== //
+export default startApp;
   // ======= EEND DO NOT EDIT ========= //
---
+++ data.mjs
@@ -0,0 +1,35 @@
+let data = {
+  //mtn
+  "0703": "mtn.svg",
+  "0803": "mtn.svg",
+  "0806": "mtn.svg",
+  "0706": "mtn.svg",
+  "0810": "mtn.svg",
+  "0813": "mtn.svg",
+  "0816": "mtn.svg",
+  "0903": "mtn.svg",
+  "0903": "mtn.svg",
+  //glo
+  "0805": "glo.svg",
+  "0807": "glo.svg",
+  "08011": "glo.svg",
+  "0705": "glo.svg",
+  "08015": "glo.svg",
+  "0905": "glo.svg",
+  //airtel
+  "0802": "airtel.svg",
+  "0808": "airtel.svg",
+  "0812": "airtel.svg",
+  "0701": "airtel.svg",
+  "0708": "airtel.svg",
+  "0902": "airtel.svg",
+  "0907": "airtel.svg",
+  "0901": "airtel.svg",
+  //9mobile
+  "0809": "9mobile.svg",
+  "0817": "9mobile.svg",
+  "0818": "9mobile.svg",
+  "0908": "9mobile.svg",
+  "0909": "9mobile.svg"
+}
+export default data;
