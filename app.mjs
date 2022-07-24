21e54bdfa879ac2bc0c26e1b6b22d1360d3b4047
Ubong Ndoh <ubongndoh@gmail.com>
2022-07-24 22:32:37 +0000

    implement the functionality to display network logo on input of number

--- app.mjs
+++ app.mjs
@@ -1,37 +1,44 @@
-import data from './data.mjs'
+import num from './data.mjs'
 
 function startApp() {
   // Your entire app should not necessarily be coded inside this 
   // single function (though there's no penalty for that), 
   // so create and use/call additional functions from here
   // pls remove the below and make some magic in here!
+
+
+  //
   console.log('make magic in here!');
-const screen = document.querySelector('.screen');
-
-  form.onsubmit = function(e) {
-    e.preventDefault();
-    var input_value = document.form.PhoneNumber.value;
-    //console.log(select);
-    input_value.addEventListener('keyup', checkCarrier)
+  const message = document.querySelector('.message');
 
 
-    function checkCarrier(e) {
+
+  // form.onsubmit = function(e) {
+  //   e.preventDefault();
+  //var input_value = document.form.PhoneNumber.value;
+  const input = document.getElementById("number");
+  //console.log(input);
+  input.addEventListener('keyup', checkCarrier)
+  function checkCarrier(e) {
     let x = e.target.value;
+    //console.log(x)
     // 
     if (x.startsWith('+234')) {
-        x = x.replace('+234', '0')
+      x = x.replace('+234', '0')
     }
     for (let item of Object.keys(num)) {
-        if (x.startsWith(item)) {
-            screen.innerHTML = `<img src=${num[item]} alt='logo'>`;
+      //console.log(num)
+      if (x.startsWith(item)) {
+        message.innerHTML = `<img src=${num[item]} alt='logo'>`;
 
-        }
+      }
 
     }
 
-}
-
   }
+
+
+  // }
 
 
   const header = document.querySelector('h2');
